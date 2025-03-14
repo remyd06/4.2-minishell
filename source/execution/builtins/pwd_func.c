51b8dc8dd74a3a7ec06a82232e43d74c8c5fb371
remyd06 <remy.dedola@outlook.com>
2025-03-14 16:04:29 +0100

    Modify PWD func (not finished).

--- source/execution/builtins/pwd_func.c
+++ source/execution/builtins/pwd_func.c
@@ -6,7 +6,7 @@
 /*   By: rdedola <rdedola@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 20:00:32 by rdedola           #+#    #+#             */
-/*   Updated: 2025/03/14 14:25:58 by rdedola          ###   ########.fr       */
+/*   Updated: 2025/03/14 15:29:20 by rdedola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 void	pwd_func(t_env *env)
 {
-	printf("%s\n", search_var("PWD", env));
+	char	*pwd;
+
+	pwd = getcwd(NULL, 0);
+	if (!pwd)
+	{
+		printf(RED"Failed to get current working directory!\n"ENDCL);
+		return (1);
+	}
+	else
+	{
+		printf("%s\n", pwd);
+		free(pwd)
+	}
+	return (0);
 }
