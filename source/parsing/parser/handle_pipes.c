9f5db42ca73865005d1909e65d143e7ae5a6fe6d
Rémy <158462966+remyd06@users.noreply.github.com>
2025-03-16 20:12:09 +0100

    Handle pipe FIX.

--- source/parsing/parser/handle_pipes.c
+++ source/parsing/parser/handle_pipes.c
@@ -21,6 +21,11 @@ void	loop(t_ms *ms, int *i, t_bool *is_pipe)
 		while (ms->lexer.tokens[*i] == WSPACE)
 			(*i)++;
 		if (ms->lexer.tokens[*i] != END && (ms->lexer.tokens[*i] == WORD
+				|| ms->lexer.tokens[*i] == DOLLAR
+				|| ms->lexer.tokens[*i] == REDIN
+				|| ms->lexer.tokens[*i] == REDOUT
+				|| ms->lexer.tokens[*i] == HEREDOC
+				|| ms->lexer.tokens[*i] == APPEND_REDIR
 				|| ms->lexer.tokens[*i] == SINGLE_QUOTE
 				|| ms->lexer.tokens[*i] == DOUBLE_QUOTE))
 		{
